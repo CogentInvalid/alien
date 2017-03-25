e9bfd08586befbead0525687170c064212374c55
CogentInvalid <pchipman@email.arizona.edu>
2017-03-25 13:45:10 -0700

    backgrounds

--- Assets/Components/CameraScript.cs
+++ Assets/Components/CameraScript.cs
@@ -6,20 +6,21 @@ public class CameraScript : MonoBehaviour {
 
 	public float speed = 3;
 	public Vector2 offset;
+
+	public Vector3 pos;
 	
 	void Start () {
-		
+		pos = transform.position;
 	}
 	
 	void Update () {
 		if (target != null) {
-			Vector3 pos = transform.position;
 			Vector2 targetPos = target.transform.position;
 
 			pos.x -= (pos.x - targetPos.x + offset.x)*speed*Time.deltaTime;
 			pos.y -= (pos.y - targetPos.y + offset.y)*speed*Time.deltaTime;
 
-			transform.position = pos;
+			transform.position = new Vector3(Mathf.Round(pos.x*32)/32, Mathf.Round(pos.y*32)/32, pos.z);
 		}
 	}
 }
