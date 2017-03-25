3456a9941fe3341ba709a8a4733cf8183a797c76
CogentInvalid <pchipman@email.arizona.edu>
2017-03-24 20:39:55 -0700

    First commit

---
+++ Assets/Components/PlatformerController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class PlatformerController : MonoBehaviour {
+
+	private Rigidbody2D phys;
+
+	public float speed = 5;
+	public float accel = 5;
+	public float friction = 8;
+
+	public float jumpForce = 5;
+
+	public float lowGravScale = 5;
+	public float highGravScale = 5;
+	bool highGravity = false;
+
+	private bool onGround;
+	private float groundTime = 0.1f;
+	private float groundTimer;
+
+	Vector2 vel;
+	
+	void Start () {
+		phys = GetComponent<Rigidbody2D>();
+		groundTimer = groundTime;
+	}
+	
+	void Update () {
+
+		groundTimer -= Time.deltaTime;
+		if (groundTimer <= 0) onGround = false;
+
+		Vector2 moveDir = new Vector2(0, 0);
+		if (Input.GetButton("Up")) moveDir.y++;
+		if (Input.GetButton("Down")) moveDir.y--;
+		if (Input.GetButton("Left")) moveDir.x--;
+		if (Input.GetButton("Right")) moveDir.x++;
+
+		//movement
+		float xMove = 0; float yMove = 0;
+		if (moveDir.x != 0) xMove = -(vel.x - speed*moveDir.x) * accel*Time.deltaTime;
+		//if (moveDir.y != 0) yMove = -(vel.y - speed*moveDir.y) * accel*Time.deltaTime;
+		vel.x += xMove; vel.y += yMove;
+
+		//friction
+		Vector2 fric = new Vector2();
+		if (xMove == 0) vel.x -= vel.x*friction*Time.deltaTime;
+
+		//jumping
+		if (Input.GetButtonDown("Jump") && onGround) {
+			vel.y = jumpForce;
+			onGround = false;
+			highGravity = false;
+		}
+
+		if (Input.GetButtonUp("Jump")) highGravity = true;
+
+		if (vel.y < 0) highGravity = false;
+
+		//gravity
+		if (highGravity)
+			vel.y -= highGravScale * Time.deltaTime;
+		else
+			vel.y -= lowGravScale * Time.deltaTime;
+
+		phys.velocity = vel;
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision) {
+		CheckHitGround(collision);
+	}
+
+	private void OnCollisionStay2D(Collision2D collision) {
+		CheckHitGround(collision);
+	}
+
+	void CheckHitGround(Collision2D collision) {
+		if (collision.contacts[0].normal.y > 0.5f) {
+			if (onGround) vel.y = -0.1f;
+			onGround = true;
+			groundTimer = groundTime;
+		}
+	}
+
+}
---
+++ Assets/Components/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float maxSpeed = 3;
+	public float friction = 6;
+	public bool airControl = true;
+
+	public float lowGrav = 8;
+	public float highGrav = 20;
+	public float jumpForce = 1;
+
+	public Rigidbody2D phys;
+
+	public bool onGround;
+	
+	void Start () {
+		phys = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+
+		float accel = 20;
+		if (!onGround) accel = 12;
+
+		float maxSpeed = this.maxSpeed;
+
+		Vector2 vel = phys.velocity;
+
+		//x-movement
+		if (Input.GetAxis("Horizontal")<0 && vel.x > -maxSpeed)
+			vel.x -= accel*Time.deltaTime;
+		if (Input.GetAxis("Horizontal")>0 && vel.x < maxSpeed)
+			vel.x += accel*Time.deltaTime;
+
+		//friction
+		if (onGround) {
+			if (vel.x > 0) vel.x -= (friction*Time.deltaTime);
+			else if (vel.x < 0) vel.x += (friction*Time.deltaTime);
+			if (vel.x > -(friction*Time.deltaTime) && vel.x < (friction*Time.deltaTime)) vel.x = 0;
+		}
+
+		//jumping
+		if (Input.GetButtonDown("Jump")) {
+			Debug.Log("jump " + onGround);
+			if (onGround) {
+				vel.y = jumpForce;
+				onGround = false;
+			}
+		}
+
+		onGround = false;
+
+		//jump holding/falling
+		if (vel.y < 0) {
+			phys.gravityScale = lowGrav;
+		} else {
+			if (Input.GetButton("Jump")) phys.gravityScale = lowGrav;
+				else phys.gravityScale = highGrav;
+			if (!airControl) phys.gravityScale = lowGrav;
+		}
+
+		phys.velocity = vel;
+
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.contacts[0].normal.y > 0) {
+			Debug.Log("yes");
+			onGround = true;
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D col) {
+		if (col.contacts[0].normal.y > 0) {
+			Debug.Log("yes2");
+			onGround = true;
+		}
+	}
+
+}
