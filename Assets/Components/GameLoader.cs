ab058b8a09ba6b82b6901e4bb377965b7616ffc8
CogentInvalid <pchipman@email.arizona.edu>
2017-03-26 09:32:26 -0700

    thing

---
+++ Assets/Components/GameLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLoader : MonoBehaviour {
+
+	void Awake () {
+		if (!SceneManager.GetSceneByName("game").isLoaded) {
+			SceneManager.LoadScene("game", LoadSceneMode.Additive);
+		}
+	}
+
+}
---
+++ Assets/Components/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public Scene currentScene;
+
+	void Start() {
+		SceneManager.sceneLoaded += LevelLoaded;
+	}
+
+	public void LoadLevel(string mapName) {
+		SceneManager.LoadScene(mapName, LoadSceneMode.Additive);
+	}
+
+	void LevelLoaded(Scene newScene, LoadSceneMode loadMode) {
+		SceneManager.UnloadScene(currentScene.name);
+		currentScene = newScene;
+		SceneManager.SetActiveScene(newScene);
+	}
+}
